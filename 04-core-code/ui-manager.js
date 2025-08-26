53dae2f664496ae922826e4030f042437e25ccad
tmt371 <124442136+tmt371@users.noreply.github.com>
2025-08-26 12:43:23 +1000

    Update ui-manager.js

--- 04-core-code/ui-manager.js
+++ 04-core-code/ui-manager.js
@@ -23,20 +23,31 @@ export class UIManager {
             const { rollerBlindItems } = state.quoteData;
             const { activeCell } = state.ui;
 
-            this.resultsTableBody.innerHTML = rollerBlindItems.map((item, index) => {
-                const isWHighlighted = index === activeCell.rowIndex && activeCell.column === 'width';
-                const isHHighlighted = index === activeCell.rowIndex && activeCell.column === 'height';
-                
-                // [修改] 加入 data-row-index, 並為儲存格加入 data-column
-                return `
-                    <tr data-row-index="${index}">
-                        <td data-column="width" class="${isWHighlighted ? 'highlighted-cell' : ''}">${item.width || ''}</td>
-                        <td data-column="height" class="${isHHighlighted ? 'highlighted-cell' : ''}">${item.height || ''}</td>
-                        <td data-column="TYPE">${(item.width || item.height) ? (item.fabricType || '') : ''}</td>
-                        <td data-column="Price" class="text-right">${item.linePrice ? '$' + item.linePrice.toFixed(2) : ''}</td>
-                    </tr>
-                `;
-            }).join('');
+            if (rollerBlindItems.length === 0) {
+                this.resultsTableBody.innerHTML = `<tr><td colspan="4" style="color: #888;">Please enter dimensions to begin...</td></tr>`;
+            } else {
+                this.resultsTableBody.innerHTML = rollerBlindItems.map((item, index) => {
+                    const isWHighlighted = index === activeCell.rowIndex && activeCell.column === 'width';
+                    const isHHighlighted = index === activeCell.rowIndex && activeCell.column === 'height';
+                    
+                    // [新增] 根據 fabricType 決定要使用的 CSS class
+                    let typeClass = '';
+                    if (item.fabricType === 'BO1') {
+                        typeClass = 'type-bo1';
+                    } else if (item.fabricType === 'SN') {
+                        typeClass = 'type-sn';
+                    }
+
+                    return `
+                        <tr data-row-index="${index}">
+                            <td data-column="width" class="${isWHighlighted ? 'highlighted-cell' : ''}">${item.width || ''}</td>
+                            <td data-column="height" class="${isHHighlighted ? 'highlighted-cell' : ''}">${item.height || ''}</td>
+                            <td data-column="TYPE" class="${typeClass}">${(item.width || item.height) ? (item.fabricType || '') : ''}</td>
+                            <td data-column="Price" class="text-right">${item.linePrice ? '$' + item.linePrice.toFixed(2) : ''}</td>
+                        </tr>
+                    `;
+                }).join('');
+            }
         }
     }
 }
