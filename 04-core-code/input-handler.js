7c3d72951e8e24c647a6507a76344636846f1967
tmt371 <124442136+tmt371@users.noreply.github.com>
2025-08-26 18:27:40 +1000

    Update input-handler.js

--- 04-core-code/input-handler.js
+++ 04-core-code/input-handler.js
@@ -37,8 +37,16 @@ export class InputHandler {
                 const column = target.dataset.column;
                 
                 if (isHeader) {
-                    // [修改] 現在也會發布 Price 表頭的點擊事件
-                    this.eventAggregator.publish('tableHeaderClicked', { column });
+                    // --- [修改開始] ---
+                    // 根據架構文件，我們需要發布一個更具語義的事件來觸發價格計算
+                    if (column === 'Price') {
+                        // 當 Price 表頭被點擊時，發布專門的計算請求事件
+                        this.eventAggregator.publish('userRequestedPriceCalculation');
+                    } else {
+                        // 對於其他表頭（例如 TYPE），維持原有的通用事件
+                        this.eventAggregator.publish('tableHeaderClicked', { column });
+                    }
+                    // --- [修改結束] ---
                 } else {
                     const rowIndex = target.parentElement.dataset.rowIndex;
                     this.eventAggregator.publish('tableCellClicked', { 
